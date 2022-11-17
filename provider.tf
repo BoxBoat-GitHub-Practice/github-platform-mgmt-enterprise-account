47dfe7dacddde631995beac338ec071b328bc94f
jay-rodriguez <jay@boxboat.com>
2022-11-17 15:18:37 -0600

    chore: remove versions and include provider file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "github" {
   owner = var.owner
 }
+terraform {
+  required_version = "1.3.4"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.8.0"
+    }
+  }
+}
