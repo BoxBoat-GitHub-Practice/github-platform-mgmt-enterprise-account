e55b471022890cd0a3da12d252e2d5e0ddf8be87
jay-rodriguez <jay@boxboat.com>
2022-12-06 15:08:44 -0600

    chore: remove space from var names

--- modules/settings/actions/variables.tf
+++ modules/settings/actions/variables.tf
@@ -1,5 +1,5 @@
 variable "org_allowed_actions" {}
 variable "org_actions_enabled_repositories" {}
-variable "org_actions_github_owned_allowed " {}
-variable "org_actions_verified_allowed " {}
-variable "org_actions_allowed_patterns " {}
+variable "org_actions_github_owned_allowed" {}
+variable "org_actions_verified_allowed" {}
+variable "org_actions_allowed_patterns" {}
