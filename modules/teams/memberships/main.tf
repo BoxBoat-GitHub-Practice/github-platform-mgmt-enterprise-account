5145126fda58f682db04c73cfc7a03ada60dcdc9
jay-rodriguez <jay@boxboat.com>
2022-12-06 16:33:03 -0600

    chore: reinclude dynamic block

--- modules/teams/memberships/main.tf
+++ modules/teams/memberships/main.tf
@@ -1,7 +1,11 @@
 # manages team members
-resource "github_team_membership" "iac_team_membership" {
+dynamic "github_team_membership" {
   team_id  = var.team_id
   for_each = var.team_member_list
-  username = each.value.team_member_username
-  role     = each.value.team_member_role
+  content {
+    username = github_team_membership.value.team_member_username
+    role     = github_team_membership.value.team_member_role
+  }
+  # username = each.value.team_member_username
+  # role     = each.value.team_member_role
 }
