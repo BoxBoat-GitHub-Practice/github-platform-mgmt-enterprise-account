894ab593f694f16ca6c01c195acbb960f33386b4
jay-rodriguez <jay@boxboat.com>
2022-12-16 11:42:39 -0600

    chore: added dynamic allowed actions config

--- modules/settings/actions/main.tf
+++ modules/settings/actions/main.tf
@@ -6,8 +6,19 @@ resource "github_actions_organization_permissions" "iac_org_actions_permissions"
     patterns_allowed     = var.org_actions_allowed_patterns
     verified_allowed     = var.org_actions_verified_allowed
   }
-  # enabled_repositories_config {
-  #   repository_ids = [github_repository.iac_repo.repo_id]
+  dynamic "allowed_actions_config" {
+    for_each = var.org_allowed_actions == "selected" ? ["allowed_actions_config"] : []
+    content {
+      github_owned_allowed = var.org_actions_github_owned_allowed
+      patterns_allowed     = var.org_actions_allowed_patterns
+      verified_allowed     = var.org_actions_verified_allowed
+    }
+  }
+  # dynamic "enabled_repositories_config" {
+  #   for_each = var.org_actions_enabled_repositories == "selected" ? ["enabled_repositories_config"] : []
+  #   content {
+  #     repository_ids = module.iac_repo["github-platform-mgmt-organization-account"].github_repository.iac_repo
+  #   }
   # }
 }
 
--- modules/settings/actions/variables.tf
+++ modules/settings/actions/variables.tf
@@ -1,5 +1,23 @@
-variable "org_allowed_actions" {}
-variable "org_actions_enabled_repositories" {}
-variable "org_actions_github_owned_allowed" {}
-variable "org_actions_verified_allowed" {}
-variable "org_actions_allowed_patterns" {}
+variable "org_allowed_actions" {
+  type        = string
+  description = "The permissions policy that controls the actions that are allowed to run. Can be one of: all, local_only, or selected."
+}
+variable "org_actions_enabled_repositories" {
+  type        = string
+  description = "The policy that controls the repositories in the organization that are allowed to run GitHub Actions. Can be one of: all, none, or selected"
+}
+variable "org_actions_github_owned_allowed" {
+  type        = bool
+  default     = true
+  description = "Whether GitHub-owned actions are allowed in the organization."
+}
+variable "org_actions_verified_allowed" {
+  type        = bool
+  default     = true
+  description = "Whether actions in GitHub Marketplace from verified creators are allowed."
+}
+variable "org_actions_allowed_patterns" {
+  type        = list(any)
+  default     = ["boxboat/terraform-azure-actions@*"]
+  description = "Specifies a list of string-matching patterns to allow specific action(s)."
+}
