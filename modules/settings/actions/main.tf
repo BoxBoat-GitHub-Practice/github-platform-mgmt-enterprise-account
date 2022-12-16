c38efa4ded208876c5da4cb79b9e3fc2cd6fc9c6
jay-rodriguez <jay@boxboat.com>
2022-12-16 12:20:19 -0600

    chore: remove old allowed actions config block

--- modules/settings/actions/main.tf
+++ modules/settings/actions/main.tf
@@ -1,11 +1,6 @@
 resource "github_actions_organization_permissions" "iac_org_actions_permissions" {
   allowed_actions      = var.org_allowed_actions
   enabled_repositories = var.org_actions_enabled_repositories
-  allowed_actions_config {
-    github_owned_allowed = var.org_actions_github_owned_allowed
-    patterns_allowed     = var.org_actions_allowed_patterns
-    verified_allowed     = var.org_actions_verified_allowed
-  }
   dynamic "allowed_actions_config" {
     for_each = var.org_allowed_actions == "selected" ? ["allowed_actions_config"] : []
     content {
